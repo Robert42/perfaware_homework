017d125c13b7d6357fe561ccbf503683e3d45a53
Robert <robert.3dgamedev@gmail.com>
2023-03-03 13:07:25 +0100

    storing the sha256 sums

--- part11/test.sh
+++ part11/test.sh
@@ -1,3 +1,6 @@
 nasm listing_0037_single_register_mov.asm
 nasm listing_0038_many_register_mov.asm
 
+sha256sum listing_0037_single_register_mov > binary.sha256
+sha256sum listing_0038_many_register_mov >> binary.sha256
+sha256sum -c binary.sha256
