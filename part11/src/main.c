0e1057c1adb80e130fecc6dccd745270ece1f691
Robert <robert.3dgamedev@gmail.com>
2023-03-03 13:23:39 +0100

    dumping the file bytes

--- part11/src/main.c
+++ part11/src/main.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUF_SIZE 4096
+unsigned char bytes[BUF_SIZE] = {};
+int num_bytes = 0;
+
 int main(int argc, char** argv)
 {
-  // read filename
-  const char* program = argv[0];
+  // get filename from args
+  const char* const program = argv[0];
   if(argc != 2)
   {
     fprintf(stderr, "usage: %s [FILE]\n", program);
     exit(1);
   }
+  const char* const filepath = argv[1];
+
+  // read file
+  {
+    FILE* f = fopen(filepath, "rb");
+    if(f == NULL)
+    {
+      fprintf(stderr, "Could not open: <%s> for reading\n", filepath);
+      exit(1);
+    }
+    num_bytes = fread(bytes, 1, BUF_SIZE, f);
+    fclose(f);
+    if(num_bytes >= BUF_SIZE)
+    {
+      fprintf(stderr, "File <%s> is too large to fit to the buffer (%i)\n", filepath, BUF_SIZE);
+      exit(1);
+    }
+  }
+
+  // output
+  printf("; %s\n", filepath);
+
+  for(int i=0; i<num_bytes; ++i)
+    printf("%02x ", bytes[i]);
 }
