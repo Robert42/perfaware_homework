382df0559b5338cdb8125e00f56276fa14531a71
Robert <robert.3dgamedev@gmail.com>
2023-03-05 21:06:25 +0100

    errorfix

--- 8086_simulator/test.sh
+++ 8086_simulator/test.sh
@@ -10,7 +10,7 @@ nasm listing_0040_challenge_movs.asm
 sha256sum listing_0037_single_register_mov > binary.sha256
 sha256sum listing_0038_many_register_mov >> binary.sha256
 sha256sum listing_0039_more_movs >> binary.sha256
-sha256sum listing_0040_challenge_movs.asm >> binary.sha256
+sha256sum listing_0040_challenge_movs >> binary.sha256
 sha256sum -c binary.sha256
 
 cd src
@@ -18,12 +18,12 @@ cd src
 ./sim8086 ../listing_0037_single_register_mov  > listing_0037_single_register_mov.asm
 ./sim8086 ../listing_0038_many_register_mov  >  listing_0038_many_register_mov.asm
 ./sim8086 ../listing_0039_more_movs  >  listing_0039_more_movs.asm
-./sim8086 ../listing_0040_challenge_movs.asm  >  listing_0040_challenge_movs.asm
+./sim8086 ../listing_0040_challenge_movs  >  listing_0040_challenge_movs.asm
 
 nasm listing_0037_single_register_mov.asm
 nasm listing_0038_many_register_mov.asm
 nasm listing_0039_more_movs.asm
-nasm listing_0040_challenge_movs.asm.asm
+nasm listing_0040_challenge_movs.asm
 
 cp ../binary.sha256 .
 sha256sum -c --ignore-missing binary.sha256
