b6d8eae43db2c62f7b8a2fa9446825fe29fa43a9
Robert <robert.3dgamedev@gmail.com>
2023-03-05 12:48:49 +0100

    assembled the new listing

--- 8086_simulator/test.sh
+++ 8086_simulator/test.sh
@@ -2,9 +2,11 @@ set -e
 
 nasm listing_0037_single_register_mov.asm
 nasm listing_0038_many_register_mov.asm
+nasm listing_0039_more_movs.asm
 
 sha256sum listing_0037_single_register_mov > binary.sha256
 sha256sum listing_0038_many_register_mov >> binary.sha256
+sha256sum listing_0039_more_movs >> binary.sha256
 sha256sum -c binary.sha256
 
 cd src
@@ -16,7 +18,7 @@ nasm listing_0037_single_register_mov.asm
 nasm listing_0038_many_register_mov.asm
 
 cp ../binary.sha256 .
-sha256sum -c binary.sha256
+sha256sum -c --ignore-missing binary.sha256
 
 rm listing_*
 rm *.sha256
