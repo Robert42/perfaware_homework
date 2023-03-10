cb063c6ca0156af110a06fc8c336db2ee8bde673
Robert <robert.3dgamedev@gmail.com>
2023-03-10 20:03:10 +0100

    working on listing 41 again

--- 8086_simulator/test.sh
+++ 8086_simulator/test.sh
@@ -30,8 +30,8 @@ nasm listing_0039_more_movs.asm
 ./sim8086 ../listing_0040_challenge_movs  >  listing_0040_challenge_movs.asm
 nasm listing_0040_challenge_movs.asm
 
-#./sim8086 ../listing_0041_add_sub_cmp_jnz  >  listing_0041_add_sub_cmp_jnz.asm
-#nasm listing_0041_add_sub_cmp_jnz.asm
+./sim8086 ../listing_0041_add_sub_cmp_jnz  >  listing_0041_add_sub_cmp_jnz.asm
+nasm listing_0041_add_sub_cmp_jnz.asm
 
 cp ../binary.sha256 .
 sha256sum -c --ignore-missing binary.sha256
